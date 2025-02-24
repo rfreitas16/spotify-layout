5eb70f3f00f28c40e17b3ff631b9bcede3968c64
rfreitas16 <rfreitas16@hotmail.com>
2025-02-24 14:09:25 -0300

    ajustes media queries

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Projeto clone spotify
+Requirements: For simulate api test: json-server
+    PS: for use get URL querystrings:  ```_like=``` json-server version needs @0.17.4 from work.
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById("result-artist");
+const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
@@ -7,16 +7,19 @@ function requestApi(searchTerm) {
     fetch(url)
         .then((response) => response.json())
         .then((result) => displayResults(result))
+        .catch(error => console.error('ERROR 404', error))
 }
 
 function displayResults(result) {
     resultPlaylist.classList.add("hidden")
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
+    const artistBanner = document.getElementById('artist-banner');
 
     result.forEach(element => {
         artistName.innerText = element.name;
         artistImage.src = element.urlImg;
+        artistBanner.bannerImg;
     });
 
     resultArtist.classList.remove('hidden');
